8a00acfe4f9f737586ba2d2174e7181a80a9c8a2
Layona-x <109739779+Layona-x@users.noreply.github.com>
2022-10-23 12:43:48 +0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,34 @@
+<script setup>
+  import Navbar from './components/Navbar.vue'
+  import Profile from './components/Profile.vue'
+</script>
 
+<script>
+  import axios from 'axios'
+  let lanyardUrl = "https://api.lanyard.rest/v1/users/920315243106795570"
+export default {
+  data () {
+    return {
+      lanyard:null,
+      info:{
+        durum:null,
+        username:null,
+        avatar:null,
+        discriminator:null,
+        id:null,
+      },
+    }
+  },
+  created: function(){
+    axios.get(lanyardUrl).then((res)=>{
+    this.lanyard = res.data.data
+    this.info.username = this.lanyard.discord_user.username
+    console.log(this.lanyard)
+    })
+  }
+}
+</script>
+<template>
+<Navbar title="Decs"/>
+<h1 id="profile_username"> {{ info.username }} </h1>
+</template>
